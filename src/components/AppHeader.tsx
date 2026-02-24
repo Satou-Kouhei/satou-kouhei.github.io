5446642a4b8f14b1cfcc2202b37b3ce001c3b7f9
satou-kouhei <kou.3145.h2@gmail.com>
2026-02-24 22:21:21 +0900

    ヘッダーの枠組みを作成（内容は仮置き）

--- src/components/AppHeader.tsx
+++ src/components/AppHeader.tsx
@@ -1,13 +1,20 @@
-function Header () {
+function AppHeader () {
     return (
         <>
-            <Header>
-                <nav>
-                    
+            <header>
+                <h1 className='page-title'>さとうこうへいのポートフォリオサイト（仮）</h1>
+                <h2 className='intoroduce'>さとう　こうへい</h2>
+                <p className='purpose'>ポートフォリオサイトです。</p>
+                <nav className='header-nav'>
+                    <ul>
+                        <li><a href="#top">TOP</a></li>
+                        <li><a href="#about">ABOUT</a></li>
+                        <li><a href="#contents">CONTENTS</a></li>
+                    </ul>
                 </nav>
-            </Header>
+            </header>
         </>
     );
 }
 
-export default Header;
+export default AppHeader;

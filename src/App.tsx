322357ea765daa433b092666f270132bd79ab643
satou-kouhei <kou.3145.h2@gmail.com>
2026-02-24 18:30:52 +0900

    スペルミス修正

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,15 @@
 import './App.css'
-import Header from './components/AppHeader'
-import Footer from './components/Footer'
+import AppHeader from './components/AppHeader'
+import AppFooter from './components/AppFooter'
 import Contents from './components/Contents'
 
 function App() {
 
   return (
     <>
-      <Header />
+      <AppHeader />
       <Contents />
-      <Footer />
+      <AppFooter />
     </>
   )
 }

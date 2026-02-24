8d56579eb81636a67d98b43dd1bcb419f6cff215
satou-kouhei <kou.3145.h2@gmail.com>
2026-02-24 22:47:12 +0900

    フッター部分の枠組みを作成

--- src/components/AppFooter.tsx
+++ src/components/AppFooter.tsx
@@ -1,6 +1,19 @@
 function AppFooter () {
     return (
-        <></>
+        <>
+            <footer>
+                <nav className='footer-nav'>
+                    <ul>
+                        <li><a href="#top">TOP</a></li>
+                        <li><a href="#about">ABOUT</a></li>
+                        <li><a href="#contents">CONTENTS</a></li>
+                    </ul>
+                </nav>
+                <p>X：<a className='sns-link' href="https://x.com/kou_forpost">https://x.com/kou_forpost</a></p>
+                <p>Git Hub：<a className='sns-link' href="https://github.com/Satou-Kouhei">https://github.com/Satou-Kouhei</a></p>
+                <p className='copyright'>© 2026 さとうこうへい</p>
+            </footer>
+        </>
     )
 }
 

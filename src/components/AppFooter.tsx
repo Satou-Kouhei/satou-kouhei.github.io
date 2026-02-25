7d85694c77f3e67f25e354a49081a48e149b65b4
satou-kouhei <kou.3145.h2@gmail.com>
2026-02-25 22:32:57 +0900

    フィッター部分のスタイルを仮置き

--- src/components/AppFooter.tsx
+++ src/components/AppFooter.tsx
@@ -1,16 +1,18 @@
 function AppFooter () {
     return (
         <>
-            <footer>
-                <nav className='footer-nav'>
-                    <ul>
+            <footer className=''>
+                <nav className='footer-nav flex flex-row'>
+                    <ul className='flex flex-col'>
                         <li><a href="#top">TOP</a></li>
                         <li><a href="#about">ABOUT</a></li>
                         <li><a href="#contents">CONTENTS</a></li>
                     </ul>
+                    <aside className='sns-link'>
+                        <p>X：<a className='sns-link' href="https://x.com/kou_forpost">https://x.com/kou_forpost</a></p>
+                        <p>Git Hub：<a className='sns-link' href="https://github.com/Satou-Kouhei">https://github.com/Satou-Kouhei</a></p>
+                    </aside>
                 </nav>
-                <p>X：<a className='sns-link' href="https://x.com/kou_forpost">https://x.com/kou_forpost</a></p>
-                <p>Git Hub：<a className='sns-link' href="https://github.com/Satou-Kouhei">https://github.com/Satou-Kouhei</a></p>
                 <p className='copyright'>© 2026 さとうこうへい</p>
             </footer>
         </>

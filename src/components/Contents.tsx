b79e14d5cc87a14407e4b964bce5436d4a39f93b
satou-kouhei <kou.3145.h2@gmail.com>
2026-02-25 11:09:40 +0900

    メイン部分の枠組みを作成

--- src/components/Contents.tsx
+++ src/components/Contents.tsx
@@ -1,6 +1,29 @@
 function Contents () {
     return (
         <>
+            <main>
+                <section id='top' className='top'>
+                    <article className='greeting'>
+                        <h2>挨拶</h2>
+                        {/* あいさつなど */}
+                    </article>
+
+                </section>
+
+                <section id='about' className='about'>
+                    <article className='about-me'>
+                        <h2>自己紹介</h2>
+                        {/* 自己紹介など */}
+                    </article>
+                </section>
+
+                <section id='contents' className='contents'>
+                    <article className='projects'>
+                        <h2>制作物</h2>
+                        {/* 作成した制作物が増えたら、項目を増やす */}
+                    </article>
+                </section>
+            </main>
         </>
     )
 }

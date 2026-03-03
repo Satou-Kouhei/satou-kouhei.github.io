d310fe2b829599c15c42e81ad8c46e0e8206ab03
satou-kouhei <kou.3145.h2@gmail.com>
2026-03-03 23:36:53 +0900

    メイン部分の位置を調整

--- src/components/Contents.tsx
+++ src/components/Contents.tsx
@@ -1,7 +1,7 @@
 function Contents () {
     return (
         <>
-            <main>
+            <main className='mt-5 flex-col-1'>
                 <section id='top' className='top'>
                     <h2>挨拶</h2>
                     <article className='greeting'>
@@ -11,28 +11,28 @@ function Contents () {
                     </article>
                 </section>
 
-                <section id='about' className='about'>
+                <section id='about' className='about mt-5'>
                     <h2>自己紹介</h2>
-                    <article className='about-me flex flex-row'>
-                        <ul>
+                    <article className='about-me flex flex-row content-between gap-3 mt-3 p-3'>
+                        <ul className='content-center text-balance justify-self-start'>
                             <li>専門学校を卒業後、就労継続支援A型事業所にて、支援スタッフとして2年半勤務。</li>
                             <li>その後、対面式のスクールにてJavaを習うが、就職支援サービスにてインフラエンジニアを勧められる。</li>
                             <li>流されるままに約1か月のCCNA講座を受講し、CCNAを取得後に派遣会社に就職。</li>
                             <li>2ヶ月ほどのコールセンター案件のあと、入場できる案件が見つからず、半年ほどで退職。</li>
-                            <li>オンラインスクールにてJavaScrilitを学習し、派遣会社に就職。</li>
+                            <li>オンラインスクールにてJavaScriptを学習し、派遣会社に就職。</li>
                             <li>富山県の現場で2年間勤め、期間満了にて退職。</li>
                         </ul>
-                        <dl className='grid grid-cols-2'>
-                            <dt className='border-b border-black'>年齢</dt>
-                            <dd className='border-b border-black'>35</dd>
-                            <dt className='border-b border-black'>出身</dt>
-                            <dd className='border-b border-black'>神奈川県</dd>
-                            <dt className='border-b border-black'>学歴</dt>
-                            <dd className='border-b border-black'>専門学校卒（福祉）</dd>
-                            <dt className='border-b border-black'>資格</dt>
-                            <dd className='border-b border-black'>なし</dd>
-                            <dt className='border-b border-black'>学習経験</dt>
-                            <dd className='border-b border-black'>
+                        <dl className='grid grid-cols-2 justify-self-end w-1/3'>
+                            <dt className='border-b border-black'>年　　齢　：</dt>
+                            <dd className='border-b border-black text-left'>35</dd>
+                            <dt className='border-b border-black'>出　　身　：</dt>
+                            <dd className='border-b border-black text-left'>神奈川県</dd>
+                            <dt className='border-b border-black'>学　　歴　：</dt>
+                            <dd className='border-b border-black text-left'>専門学校卒（福祉）</dd>
+                            <dt className='border-b border-black'>資　　格　：</dt>
+                            <dd className='border-b border-black text-left'>なし</dd>
+                            <dt className='border-b border-black'>学習経験　：</dt>
+                            <dd className='border-b border-black text-left'>
                                 <ul>
                                     <li>HTML</li>
                                     <li>CSS</li>
@@ -47,8 +47,8 @@ function Contents () {
                                     <li>SvelteKit</li>
                                 </ul>
                             </dd>
-                            <dt className='border-b border-black'>実務経験</dt>
-                            <dd className='border-b border-black'>
+                            <dt className='border-b border-black'>実務経験　：</dt>
+                            <dd className='border-b border-black text-left'>
                                 <ul>
                                     <li>Google App Script(GAS)にて、メール下書きツールを作成。（実用には至らず）</li>
                                     <li>VBAにて、複数ファイルを横断する集計作業の一部を自動化。</li>
@@ -58,21 +58,21 @@ function Contents () {
                     </article>
                 </section>
 
-                <section id='contents' className='contents'>
+                <section id='contents' className='contents mt-4'>
                     <h2>制作物</h2>
-                    <article className='projects flex flex-col gap-3 container border box-border p-1 bg-linear-to-l from-sky-300 to-violet-300'>
+                    <article className='projects grid grid-row-3 grid-cols-2 content-around gap-3 container box-border size-1/3 p-1 bg-linear-to-l from-sky-300 to-violet-300'>
                         {/* ロゴを作成したら bg-linear をはずす */}
-                        <img src="" alt="ロゴ未作成" className='site-logo box size-20' />
-                        <p className='project-name'>ポートフォリオサイト</p>
-                        <ul className='use-stack flex flex-row gap-2'>
+                        <img src="" alt="ロゴ未作成" className='site-logo row-span-3' />
+                        <p className='project-name row-span-1'>ポートフォリオサイト</p>
+                        <ul className='use-stack row-span-2 flex flex-col'>
                             <li>HTML</li>
                             <li>CSS（Tailwindcss）</li>
                             <li>JavaScript / TypeScript</li>
                             <li>Node.js</li>
                         </ul>
-                        <aside>
+                        <aside className='col-span-2'>
                             <h3>こだわりポイント</h3>
-                            <p>スクレイピングに興味があり、眺めていたサイトのソースを開いたところ、
+                            <p className='text-left'>スクレイピングに興味があり、眺めていたサイトのソースを開いたところ、
                                 7重くらいのdivタグのネストの中に内容がたったの1行というのを見たことがあります。</p>
                             <p>そこで、divタグの多用に疑問を持ったことから、このページではツールが自動で生成するもの以外のdivタグを使用していません。</p>
                         </aside>

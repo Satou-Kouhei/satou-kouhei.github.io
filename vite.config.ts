023f1daaf4515b4faa8708417b05d1d20d870e9f
satou-kouhei <kou.3145.h2@gmail.com>
2026-03-06 16:31:49 +0900

    設定を追加

--- vite.config.ts
+++ vite.config.ts
@@ -6,6 +6,7 @@ import tailwindcss from '@tailwindcss/vite'
 export default defineConfig({
   plugins: [
     react(),
-    tailwindcss()
+    tailwindcss(),
   ],
+  base: '/satou-kouhei.github.io/'
 })
